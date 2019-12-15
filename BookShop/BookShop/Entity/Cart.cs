50e7d1d068b2a6eed87da93fb8aae9ae5dbe85cb
Thien Phuc <ttran.phuc99@gmail.com>
2019-12-15 22:12:23 +0700

    add template admin, doing cart

---
+++ BookShop/BookShop/Entity/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookShop.Entity
+{
+    public class Cart
+    {
+        public List<Book> List { get; }
+        public long TotalPrice { get; set; }
+
+
+    }
+}
